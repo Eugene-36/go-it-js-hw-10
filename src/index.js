b16c3c2a77ddea09e9633a1efce3e2e7d707dc5b
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2020-12-08 21:14:38 +0200

    add new file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import css from "./css/styles.css";
-import menu from "./data/menu.json"
-import ref from "./js/refs.js"
-import cardsTemplate from "../src/templates/post.hbs"
+import menu from "./data/menu.json";
+import ref from "./js/refs.js";
+import cardsTemplate from "../src/templates/post.hbs";
 
-
-// Сделал шаблон 
+// Сделал шаблон
 const newCard = cardsTemplate(menu);
-const containerRef = document.querySelector('.menu')
-containerRef.insertAdjacentHTML('afterbegin', newCard)
-
-
+const containerRef = document.querySelector(".menu");
+containerRef.insertAdjacentHTML("afterbegin", newCard);
